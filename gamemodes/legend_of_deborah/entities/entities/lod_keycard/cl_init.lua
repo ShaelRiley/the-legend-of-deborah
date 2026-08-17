2794070e2e078bc0df2b1536a543412b50d53b53
Shael Riley <shael.riley@gmail.com>
2026-08-16 23:25:31 -0500

    Render keycards from explicit 3D hooks

--- gamemodes/legend_of_deborah/entities/entities/lod_keycard/cl_init.lua
+++ gamemodes/legend_of_deborah/entities/entities/lod_keycard/cl_init.lua
@@ -7,17 +7,42 @@ local cardMaterial = CreateMaterial("lod_keycard_opaque_v1", "UnlitGeneric", {
     ["$vertexalpha"] = "1"
 })
 
+LOD.ClientKeycards = LOD.ClientKeycards or setmetatable({}, {__mode = "k"})
+
+function ENT:Initialize()
+    LOD.ClientKeycards[self] = true
+end
+
+function ENT:OnRemove()
+    LOD.ClientKeycards[self] = nil
+end
+
 function ENT:Draw()
-    local card = PC.Cards[math.Clamp(self:GetCardIndex(), 1, 3)]
+end
+
+hook.Add("PostDrawOpaqueRenderables", "LOD_DrawKeycards", function()
     render.SetMaterial(cardMaterial)
     render.CullMode(MATERIAL_CULLMODE_NONE)
-    render.DrawBox(self:GetPos(), Angle(0, CurTime() * 45 % 360, 0), Vector(-18, -28, -3), Vector(18, 28, 3), card.color)
+    for ent in pairs(LOD.ClientKeycards) do
+        if IsValid(ent) then
+            local card = PC.Cards[math.Clamp(ent:GetCardIndex(), 1, 3)]
+            render.DrawBox(ent:GetPos(), Angle(0, CurTime() * 45 % 360, 0),
+                Vector(-18, -28, -3), Vector(18, 28, 3), card.color)
+        end
+    end
     render.CullMode(MATERIAL_CULLMODE_CCW)
+end)
 
-    local ang = Angle(0, EyeAngles().y - 90, 90)
-    cam.Start3D2D(self:GetPos() + Vector(0, 0, 38), ang, 0.12)
-        draw.RoundedBox(4, -130, -30, 260, 60, Color(16, 18, 20, 230))
-        draw.SimpleText(card.name .. " KEYCARD", "DermaLarge", 0, -8, card.color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        draw.SimpleText(card.letter .. " / " .. card.symbol, "DermaDefaultBold", 0, 18, Color(245, 245, 245), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-    cam.End3D2D()
-end
+hook.Add("PostDrawTranslucentRenderables", "LOD_DrawKeycardLabels", function()
+    for ent in pairs(LOD.ClientKeycards) do
+        if IsValid(ent) then
+            local card = PC.Cards[math.Clamp(ent:GetCardIndex(), 1, 3)]
+            local ang = Angle(0, EyeAngles().y - 90, 90)
+            cam.Start3D2D(ent:GetPos() + Vector(0, 0, 38), ang, 0.12)
+                draw.RoundedBox(4, -130, -30, 260, 60, Color(16, 18, 20, 230))
+                draw.SimpleText(card.name .. " KEYCARD", "DermaLarge", 0, -8, card.color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+                draw.SimpleText(card.letter .. " / " .. card.symbol, "DermaDefaultBold", 0, 18, Color(245, 245, 245), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+            cam.End3D2D()
+        end
+    end
+end)
