c7656ab48a907cb4b6994993c569ea8ae88ad0dc
Shael Riley <shael.riley@gmail.com>
2026-08-18 15:06:07 -0500

    Add Bio Blaster projectile entity definition

---
+++ gamemodes/legend_of_deborah/entities/entities/lod_bio_bolt/shared.lua
@@ -0,0 +1,7 @@
+ENT.Base = "base_anim"
+ENT.Type = "anim"
+ENT.PrintName = "Legend of Deborah Bio Bolt"
+ENT.Category = "The Legend of Deborah"
+ENT.Spawnable = false
+ENT.AdminOnly = false
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
