71fa4e7d85417a888859a4b708bf0d36c1a7cb11
Shael Riley <shael.riley@gmail.com>
2026-08-18 12:47:14 -0500

    Render Soldier aiming telegraph

--- gamemodes/legend_of_deborah/entities/entities/lod_hostile/cl_init.lua
+++ gamemodes/legend_of_deborah/entities/entities/lod_hostile/cl_init.lua
@@ -1,1 +1,20 @@
 include("shared.lua")
+
+local aimMaterial = Material("cable/redlaser")
+
+function ENT:Draw()
+    self:DrawModel()
+end
+
+function ENT:DrawTranslucent()
+    self:DrawModel()
+    if self:GetNW2String("LOD_Archetype", "") ~= "soldier" then return end
+    if not self:GetNW2Bool("LOD_SoldierTelegraph", false) then return end
+
+    local aim = self:GetNW2Vector("LOD_SoldierAim", vector_origin)
+    if aim == vector_origin then return end
+
+    local startPos = self:WorldSpaceCenter() + Vector(0, 0, 12) + self:GetForward() * 24
+    render.SetMaterial(aimMaterial)
+    render.DrawBeam(startPos, aim, 2.5, 0, 1, Color(255, 80, 60, 220))
+end
