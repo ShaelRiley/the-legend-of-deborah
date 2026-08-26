9b56edd6e05e403e7ff0de07f70f162177391839
Shael Riley <shael.riley@gmail.com>
2026-08-26 09:24:11 -0500

    Load melee contact client audit

--- gamemodes/legend_of_deborah/gamemode/cl_init.lua
+++ gamemodes/legend_of_deborah/gamemode/cl_init.lua
@@ -7,6 +7,7 @@ include("lod/cl_magic_hud.lua")
 include("lod/cl_dev_testing.lua")
 include("lod/cl_hit_confirm.lua")
 include("lod/cl_combat_roll_feed.lua")
+include("lod/cl_melee_contact_audit.lua")
 include("lod/cl_minimap.lua")
 -- Close rare incomplete-transfer and sparse/stair player-cell routing failures
 -- without changing the accepted cached topology/BFS architecture.
