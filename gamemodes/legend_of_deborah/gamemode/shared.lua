9f3ecf355a45291962fbb689d7148905857d0a66
Shael Riley <shael.riley@gmail.com>
2026-08-26 20:06:46 -0500

    Ship container recolor module to clients

--- gamemodes/legend_of_deborah/gamemode/shared.lua
+++ gamemodes/legend_of_deborah/gamemode/shared.lua
@@ -17,6 +17,7 @@ include("lod/sh_rng.lua")
 
 if SERVER then
     AddCSLuaFile("lod/cl_container_wayfinding_projection.lua")
+    AddCSLuaFile("lod/cl_container_section_recolor.lua")
 end
 
 -- Base gamemode's TeamBased example creates Blue/Orange/Sexy teams. Override
