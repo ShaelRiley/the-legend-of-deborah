7124c6b260245d868f03c0ba4ae9fe2c84ab6154
Shael Riley <shael.riley@gmail.com>
2026-08-27 22:41:31 -0500

    Extend staging prop presentation kinds

--- gamemodes/legend_of_deborah/entities/entities/lod_staging_prop/shared.lua
+++ gamemodes/legend_of_deborah/entities/entities/lod_staging_prop/shared.lua
@@ -3,10 +3,13 @@ ENT.Base = "base_anim"
 ENT.PrintName = "LOD Staging Prop"
 ENT.Spawnable = false
 ENT.AdminOnly = false
+ENT.AutomaticFrameAdvance = true
 
 ENT.KIND_GUIDE = 1
 ENT.KIND_PORTAL = 2
 ENT.KIND_WEAPON = 3
+ENT.KIND_SIGN = 4
+ENT.KIND_TORCH = 5
 
 function ENT:SetupDataTables()
     self:NetworkVar("Int", 0, "StageKind")
