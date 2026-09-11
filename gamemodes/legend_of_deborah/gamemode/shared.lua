a4a748e985af403045ac072d527951a367d6243b
Shael Riley <shael.riley@gmail.com>
2026-09-11 07:44:18 -0500

    Add Checkpoint D morale defense feats

--- gamemodes/legend_of_deborah/gamemode/shared.lua
+++ gamemodes/legend_of_deborah/gamemode/shared.lua
@@ -65,6 +65,7 @@ if SERVER then
     include("lod/sv_rpg_checkpoint_d_summon_feats.lua")
     include("lod/sv_magic_progression.lua")
     include("lod/sv_rpg_checkpoint_d_magic_grant_feats.lua")
+    include("lod/sv_rpg_checkpoint_d_morale_defense_feats.lua")
     include("lod/sv_workshop_distribution.lua")
     AddCSLuaFile("lod/cl_container_wayfinding_projection.lua")
     AddCSLuaFile("lod/cl_container_section_recolor.lua")
